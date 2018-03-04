c05544893c496533394ed8676d47b126aadf2d01
ikbol009@gmail.com <ikbol009@gmail.com>
2018-03-04 16:57:42 -0500

    weew

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -3,7 +3,8 @@ package test;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hi Misha");
-		System.out.println("Buy Misha");
+		System.out.println("Bye Misha");
+		System.out.println("Sell Misha");
 	}
 
 }
